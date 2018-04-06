61905dcfe03a315e253142f1ad19c21493a90aae
gearmobile <gearmobile@gmail.com>
2018-04-06 10:47:05 +0300

    vuetify - add gradient to nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-toolbar fixed app :gradient="gradient">
+  <v-toolbar class="nav" fixed app :gradient="gradient">
     <v-toolbar-side-icon></v-toolbar-side-icon>
     <v-btn icon>
       <v-icon v-html="'chevron_right'"></v-icon>
@@ -30,7 +30,11 @@ export default {
 </script>
 
 <style scoped>
-.block {
-  margin-bottom: 1rem;
-}
+  .nav {
+    background: #75a7ff; /* Old browsers */
+    background: -moz-linear-gradient(top, #75a7ff 0%, #004ecb 100%); /* FF3.6-15 */
+    background: -webkit-linear-gradient(top, #75a7ff 0%,#004ecb 100%); /* Chrome10-25,Safari5.1-6 */
+    background: linear-gradient(to bottom, #75a7ff 0%,#004ecb 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#75a7ff', endColorstr='#004ecb',GradientType=0 ); /* IE6-9 */
+  }
 </style>
